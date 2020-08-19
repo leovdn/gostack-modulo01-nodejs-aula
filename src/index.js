d8767befc8eef030e08709e040b680365e1055c1
leovdn <leo_vdn@hotmail.com>
2020-08-19 14:19:14 -0300

    created middlewares for update and delete methods

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 const express = require('express');
-const { uuid } = require('uuidv4')
+const { v4: uuid } = require('uuid')
 const app = express();
 
 app.use(express.json());
 
 const projects = []; // deletar ao colocar em produção
 
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.log(logLabel);
+
+  return next();
+}
+
+function validadeProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.'});
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validadeProjectId)
+
 app.get('/projects', (request, response) => {
   const { title } = request.query;
 
   const results = title
     ? projects.filter(project => project.title.includes(title))
-    : projects;
+    : projects
+  ;
     
 
   return response.json(results);
