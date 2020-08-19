39bfb80feff7c70d18f0352ae1ecef54c7d8e3ec
leovdn <leo_vdn@hotmail.com>
2020-08-19 13:16:14 -0300

    finished routes, update and delete; get filters working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { uuid } = require('uuidv4')
 const app = express();
 
 app.use(express.json());
@@ -7,42 +7,60 @@ app.use(express.json());
 const projects = []; // deletar ao colocar em produção
 
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query;
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ])
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+    
+
+  return response.json(results);
 })
 
 app.post('/projects', (request, response) => {
   const { title, owner} = request.body;
 
-  console.log(title, owner);
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-    'Projeto 4',
-  ])
+  const project = { id: uuid(), title, owner }
+
+  projects.push(project);
+
+  return response.json(project)
 })
 
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params;
-  console.log(id);
-  return response.json([
-    'Projeto 5',
-    'Projeto 2',
-    'Projeto 3',
-    'Projeto 4',
-  ])
+  const { title, owner} = request.body;
+
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+  
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not Found.'})
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project)
 })
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 3',
-    'Projeto 4',
-  ])
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+  
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not Found.'})
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 })
 
 app.listen(3333, () => {
