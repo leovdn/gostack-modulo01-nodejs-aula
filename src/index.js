1621bff5f8a75c0c3dcc0977a43221d6c4c393b8
leovdn <leo_vdn@hotmail.com>
2020-08-19 12:42:53 -0300

    created query and route params; created body request

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const projects = []; // deletar ao colocar em produção
+
 app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -11,6 +16,9 @@ app.get('/projects', (request, response) => {
 })
 
 app.post('/projects', (request, response) => {
+  const { title, owner} = request.body;
+
+  console.log(title, owner);
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -20,6 +28,8 @@ app.post('/projects', (request, response) => {
 })
 
 app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  console.log(id);
   return response.json([
     'Projeto 5',
     'Projeto 2',
